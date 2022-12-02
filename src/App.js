46423f296e7d48d7585270c8bd2993ff416ab8ca
andja <andja.velimirovic@gmail.com>
2022-12-02 18:44:03 +0100

    styling & firebase config

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import Register from "./components/Register";
+import Login from "./components/Login";
+
 function App() {
-  return <div className="App">{console.log("auth")}</div>;
+  return (
+    <main className="container">
+      <Register />
+      {/* <Login /> */}
+    </main>
+  );
 }
 
 export default App;
