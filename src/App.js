aa2be58cb443b735ac604374c080842a5fa1f29b
andja <andja.velimirovic@gmail.com>
2022-12-03 07:12:55 +0100

    handle logout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import Inputs from "./components/Inputs";
 import React, { useState } from "react";
 import Button from "./components/Button";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+} from "firebase/auth";
 import { auth } from "./firebase-config";
 
 function App() {
   const [registerEmail, setRegisterEmail] = useState("");
   const [registerPassword, setRegisterPassword] = useState("");
+  const [user, setUser] = useState({});
+
+  onAuthStateChanged(auth, (currentUser) => {
+    setUser(currentUser);
+  });
 
   const registerUser = async () => {
     try {
@@ -20,18 +29,37 @@ function App() {
       console.log(error.message);
     }
   };
+
+  const handleLogout = async () => {
+    await signOut(auth);
+  };
+
   const handleLogin = () => {
     console.log("login");
   };
+
   return (
     <main className="container">
-      <h2>Register User</h2>
-      <Inputs setEmail={setRegisterEmail} setPassword={setRegisterPassword} />
-      <Button text="Create Account" handleClick={registerUser} />
-      <div className="log-in">
-        <p>Already have an Acount?</p>
-        <Button text="Log In" handleClick={handleLogin} />
-      </div>
+      {user ? (
+        <>
+          <h4>{user?.email}</h4>
+          <Button text="log out" handleClick={handleLogout} />
+        </>
+      ) : (
+        <>
+          <h2>Register User</h2>
+          <Inputs
+            setEmail={setRegisterEmail}
+            setPassword={setRegisterPassword}
+          />
+          <Button text="Create Account" handleClick={registerUser} />
+          <p>{user?.email}</p>
+          <div className="log-in">
+            <p>Already have an Acount?</p>
+            <Button text="Log In" handleClick={handleLogin} />
+          </div>
+        </>
+      )}
     </main>
   );
 }
